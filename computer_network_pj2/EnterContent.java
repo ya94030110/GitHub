2d39ffcf80dd5e415df91d06b0cbe084386c34cd
b03902087 <b03902087@ntu.edu.tw>
2016-12-13 02:04:56 +0800

    build post objects`

---
+++ computer_network_pj2/EnterContent.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+public class EnterContent
+{
+	public EnterContent()
+	{
+		
+	}
+	public void work(DataOutputStream output, String Content)
+	{
+		try
+		{
+			output.writeUTF(Content);
+	        output.flush();
+	        TimeUnit.SECONDS.sleep(1);
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+}
